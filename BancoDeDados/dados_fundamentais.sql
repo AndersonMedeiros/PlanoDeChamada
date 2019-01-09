337e7db398cf50d4acb5181cdc5a0e657224ab4d
AndersonMedeiros <andersonandrade.30@gmail.com>
2019-01-09 17:59:59 -0400

    Adicionei os bairros na tabela bairro

--- BancoDeDados/dados_fundamentais.sql
+++ BancoDeDados/dados_fundamentais.sql
@@ -61,3 +61,96 @@ insert into escolaridade values (1, 'Ensino Fundamental Completo'),
 
 select * from escolaridade;
 
+
+insert into cidade values (1, 'Manaus'),
+						  (2, 'Rio Preto da Eva'),
+                          (3, 'Iranduba');
+                          
+select * from cidade;
+
+
+insert into bairro values (1, null, 'São Lázaro', 'Sul');
+
+insert into bairro values (1, null, 'Adrianópolis', 'Centro-Sul');
+
+insert into bairro values (1, null, 'Centro', 'Sul'),
+                          (1, null, 'Nossa Senhora Aparecida', 'Sul'),
+                          (1, null, 'Presidente Vargas', 'Sul'),
+                          (1, null, 'Praça 14 de Janeiro', 'Sul'),
+                          (1, null, 'Cachoeirinha', 'Sul'),
+                          (1, null, 'Raiz', 'Sul'),
+                          (1, null, 'São Francisco', 'Sul'),
+                          (1, null, 'Petrópolis', 'Sul'),
+                          (1, null, 'Japiim', 'Sul'),
+                          (1, null, 'Educandos', 'Sul'),
+                          (1, null, 'Japiim', 'Sul'),
+                          (1, null, 'Educandos', 'Sul'),
+                          (1, null, 'Santa Luzia', 'Sul'),
+                          (1, null, 'Morro da Liberdade', 'Sul'),
+                          (1, null, 'Betânia', 'Sul'),
+                          (1, null, 'Colônia Oliveira Machado', 'Sul'),
+                          (1, null, 'Crespo', 'Sul'),
+                          (1, null, 'Vila Buriti', 'Sul'),
+                          (1, null, 'Distrito Industrial I', 'Sul'),
+                          
+                          (1, null, 'Dom Pedro', 'Centro-Sul'),
+                          (1, null, 'Flores', 'Centro-Sul'),
+                          (1, null, 'Parque 10 de Novembro', 'Centro-Sul'),
+                          (1, null, 'Aleixo', 'Centro-Sul'),
+                          (1, null, 'Nossa Senhora das Graças', 'Centro-Sul'),
+                          (1, null, 'São Geraldo', 'Centro-Sul'),
+                          (1, null, 'Chapada', 'Centro-Sul'),
+                          
+                          (1, null, 'São Raimundo', 'Oeste'),
+                          (1, null, 'Glória', 'Oeste'),
+                          (1, null, 'Santo Antônio', 'Oeste'),
+                          (1, null, 'Vila da Prata', 'Oeste'),
+                          (1, null, 'Compensa', 'Oeste'),
+                          (1, null, 'São Jorge', 'Oeste'),
+                          (1, null, 'Santo Agostinho', 'Oeste'),
+                          (1, null, 'Nova Esperança', 'Oeste'),
+                          (1, null, 'Lírio do Vale', 'Oeste'),
+                          (1, null, 'Ponta Negra', 'Oeste'),
+                          (1, null, 'Tarumã', 'Oeste'),
+                          (1, null, 'Tarumã-Açu', 'Oeste'),
+                          
+                          (1, null, 'Planalto', 'Centro-Oeste'),
+                          (1, null, 'Alvorada', 'Centro-Oeste'),
+                          (1, null, 'Redenção', 'Centro-Oeste'),
+                          (1, null, 'Da Paz', 'Centro-Oeste'),
+                          
+                          (1, null, 'Coroado', 'Leste'),
+                          (1, null, 'Distrito Industrial II', 'Leste'),
+                          (1, null, 'Mauazinho', 'Leste'),
+                          (1, null, 'Colônia Antônio Aleixo', 'Leste'),
+                          (1, null, 'Puraquequara', 'Leste'),
+                          (1, null, 'Armando Mendes', 'Leste'),
+                          (1, null, 'Zumbi dos Palmares', 'Leste'),
+                          (1, null, 'São José Operário', 'Leste'),
+                          (1, null, 'Tancredo Neves', 'Leste'),
+                          (1, null, 'Jorge Teixeira', 'Leste'),
+                          (1, null, 'Gilberto Mestrinho', 'Leste'),
+                          (1, null, 'Lago Azul', 'Leste'),
+                          
+                          (1, null, 'Colônia Santo Antônio', 'Norte'),
+                          (1, null, 'Novo Israel', 'Norte'),
+                          (1, null, 'Colônia Terra Nova', 'Norte'),
+                          (1, null, 'Santa Etelvina', 'Norte'),
+                          (1, null, 'Monte das Oliveiras', 'Norte'),
+                          (1, null, 'Cidade Nova', 'Norte'),
+                          (1, null, 'Novo Aleixo', 'Norte'),
+                          (1, null, 'Cidade de Deus', 'Norte'),
+                          (1, null, 'Nova Cidade', 'Norte');
+                        
+                          
+                          
+                          
+select *from bairro group by Bairro_zona, Bairro_nome;
+
+
+
+
+
+
+
+
