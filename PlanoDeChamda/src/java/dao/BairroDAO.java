658d64b8916abfdd711e9062f24b1c750ed37b1b
AndersonMedeiros <andersonandrade.30@gmail.com>
2019-01-09 21:42:06 -0400

    Atualização com o atributo id_cidade e criação de um método que busca todos os bairros pelo id_cidade

--- PlanoDeChamda/src/java/dao/BairroDAO.java
+++ PlanoDeChamda/src/java/dao/BairroDAO.java
@@ -17,6 +17,7 @@ import java.util.ArrayList;
 public class BairroDAO {
     private final String GETBAIRROBYID = "SELECT * FROM bairro WHERE Bairro_id=?";
     private final String GETBAIRROS = "SELECT * FROM bairro";
+    private final String GETBAIRROSBYIDCID = "SELECT * FROM bairro WHERE Bairro_cid_id=?";
     Connection conn;
     PreparedStatement pstm;
     
@@ -36,6 +37,33 @@ public class BairroDAO {
                b.setId(rs.getInt("Bairro_id"));
                b.setNome(rs.getString("Bairro_nome"));
                b.setZona(rs.getString("Bairro_zona"));
+               b.setZona(rs.getString("Bairro_cid_id"));
+            }
+            ConnectionFactory.fechaConexao(conn, pstm, rs);
+        }catch(Exception e){
+            e.printStackTrace();
+            throw new RuntimeException(e.getMessage());
+        }
+        return b;
+    }
+    
+    public Bairro getBairroByIdC(int id){
+        conn = null;
+        pstm = null;
+        ResultSet rs = null;
+        Bairro b = new Bairro();
+        
+        try{
+            conn = ConnectionFactory.getConnection();
+            pstm = conn.prepareStatement(GETBAIRROBYID);
+            pstm.setInt(1, id);
+            rs = pstm.executeQuery();
+        
+            while (rs.next()) {
+               b.setId(rs.getInt("Bairro_id"));
+               b.setNome(rs.getString("Bairro_nome"));
+               b.setZona(rs.getString("Bairro_zona"));
+               b.setZona(rs.getString("Bairro_cid_id"));
             }
             ConnectionFactory.fechaConexao(conn, pstm, rs);
         }catch(Exception e){
@@ -62,6 +90,36 @@ public class BairroDAO {
                b.setId(rs.getInt("Bairro_id"));
                b.setNome(rs.getString("Bairro_nome"));
                b.setZona(rs.getString("Bairro_zona"));
+               b.setId_cid(rs.getInt("Bairro_cid_id"));
+               bairros.add(b);
+            }
+            ConnectionFactory.fechaConexao(conn, pstm, rs);
+        }catch(Exception e){
+            e.printStackTrace();
+            throw new RuntimeException(e.getMessage());
+        }
+        return bairros;
+    }
+    
+    public ArrayList<Bairro> getBairrosByCidade(int id_cid){
+        conn = null;
+        pstm = null;
+        ResultSet rs = null;
+        
+        ArrayList<Bairro> bairros = new ArrayList<Bairro>();
+        try{
+            conn = ConnectionFactory.getConnection();
+            pstm = conn.prepareStatement(GETBAIRROSBYIDCID);
+            pstm.setInt(1, id_cid);
+            
+            rs = pstm.executeQuery();
+        
+            while (rs.next()) {
+               Bairro b = new Bairro();
+               b.setId(rs.getInt("Bairro_id"));
+               b.setNome(rs.getString("Bairro_nome"));
+               b.setZona(rs.getString("Bairro_zona"));
+               b.setId_cid(rs.getInt("Bairro_cid_id"));
                bairros.add(b);
             }
             ConnectionFactory.fechaConexao(conn, pstm, rs);
