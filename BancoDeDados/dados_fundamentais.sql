43d4b3a4e8d48c30a52de445ccff4ce961af82ad
AndersonMedeiros <andersonandrade.30@gmail.com>
2019-01-21 23:07:04 -0300

    Atualização no dados inseridos no banco

--- BancoDeDados/dados_fundamentais.sql
+++ BancoDeDados/dados_fundamentais.sql
@@ -1,65 +1,71 @@
-use cigs_generico;
+use cigs;
 
-insert into postograduacao values (1, 'Marechal'),
-								  (2, 'General de Exército'),
-								  (3, 'General de Divisão'),
-								  (4, 'General de Brigada'),
-								  (5, 'Coronel'),
-								  (6, 'Tenente Coronel'),
-								  (7, 'Major'),
-								  (8, 'Capitão'),
-								  (9, '1º Tenente'),
-								 (10, '2º Tenente'),
-								 (11, 'Aspirante a Oficial'),
-								 (12, 'Subtenente'),
-								 (13, '1º Sargento'),
-								 (14, '2º Sargento'),
-								 (15, '3º Sargento'),
-								 (16, 'Cabo'),
-								 (17, 'Soldado');
+insert into postograduacao(nome) values ('Marechal'),
+								  ('General de Exército'),
+								  ('General de Divisão'),
+								  ('General de Brigada'),
+								  ('Coronel'),
+								  ('Tenente Coronel'),
+								  ('Major'),
+								  ('Capitão'),
+								  ('1º Tenente'),
+								 ('2º Tenente'),
+								 ('Aspirante a Oficial'),
+								 ('Subtenente'),
+								 ('1º Sargento'),
+								 ('2º Sargento'),
+								 ('3º Sargento'),
+								 ('Cabo'),
+								 ('Soldado do Efetivo Profissional'),
+								 ('Soldado do Efetivo Variável');
 
 select * from postograduacao;
 
-insert into qas_qms values (1, 'Arma de Infantaria'),
-						   (2, 'Arma de Cavalaria'),
-						   (3, 'Arma de Artilharia'),
-						   (4, 'Arma de Engenharia'),
-						   (5, 'Arma de Comunicações'),
-						   (6, 'Quadro de Material Bélico'),
-						   (7, 'Quadro de Engenheiros Militares'),
-						   (8, 'Quadro Auxiliar de Oficiais'),
-						   (9, 'Quadro Complementar de Oficiais'),
-						  (10, 'Serviço de Intendência'),
-						  (11, 'Serviço de Saúde'),
-						  (12, 'Serviço de Assistência Religiosa');
+insert into qasqms(nome) values ('Arma de Infantaria'),
+						   ('Arma de Cavalaria'),
+						   ('Arma de Artilharia'),
+						   ('Arma de Engenharia'),
+						   ('Arma de Comunicações'),
+						   ('Quadro de Material Bélico'),
+						   ('Quadro de Engenheiros Militares'),
+						   ('Quadro Auxiliar de Oficiais'),
+						   ('Quadro Complementar de Oficiais'),
+						   ('Serviço de Intendência'),
+						   ('Serviço de Saúde'),
+						   ('Serviço de Assistência Religiosa');
 
-select * from qas_qms;
+select * from qasqms;
 
-insert into divisaosecao values (1, 'Divisão de Pessoal (S1)'),
-							    (2, 'Divisão Administrativa (DA/S4)'),
-							    (3, 'Divisão de Ensino (DE)'),
-							    (4, 'Divisão de Alunos (Div Al)'),
-							    (5, 'Divisão de Doutrina e Pequisa (DDP)'),
-							    (6, 'Divisão Veterinária (Div Vet)'),
-							    (7, 'Seção de Operações'),
-							    (8, 'Seção de Inteligência (S2)'),
-							    (9, 'Seção de Tecnologia da Informação (STI)'),
-							   (10, 'Companhia de Comando e Serviço (CCSv)'),
-							   (11, 'Companhia Auxiliar de Ensino (CAE)'),
-							   (12, 'Comunicação Social (Com Soc)'),
-							   (13, 'Assessoria Jurídica'),
-							   (14, 'Hotel de Trânsito (HT)');
+insert into divisaosecao(nome) values ('Divisão de Pessoal (S1)'),
+							    ('Divisão Administrativa (DA/S4)'),
+							    ('Divisão de Ensino (DE)'),
+							    ('Divisão de Alunos (Div Al)'),
+							    ('Divisão de Doutrina e Pequisa (DDP)'),
+							    ('Divisão Veterinária (Div Vet)'),
+							    ('Seção de Operações'),
+							    ('Seção de Inteligência (S2)'),
+							    ('Seção de Tecnologia da Informação (STI)'),
+							   ('Companhia de Comando e Serviço (CCSv)'),
+							   ('Companhia Auxiliar de Ensino (CAE)'),
+							   ('Comunicação Social (Com Soc)'),
+							   ('Assessoria Jurídica'),
+							   ('Hotel de Trânsito (HT)');
 
 select * from divisaosecao;
 
-insert into escolaridade values (1, 'Ensino Fundamental Completo'),
-								(2, 'Ensino Médio Completo'),
-								(3, 'Ensino Superior Completo'),
-								(4, 'Pós Graduação Completa'),
-								(5, 'Mestre'),
-								(6, 'Doutor');
+insert into escolaridade(nome) values ('Ensino Fundamental Completo'),
+								('Ensino Médio Completo'),
+								('Ensino Superior Completo'),
+								('Pós Graduação Completa'),
+								('Mestre'),
+								('Doutor');
 
 select * from escolaridade;
+
+insert into estado(nome) values ('Amazonas');
+insert into estado(nome) values ('Bahia');
+
+select * from estado;
 
 
 insert into cidade values (1, 'Manaus'),
